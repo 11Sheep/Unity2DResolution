2c526290c2d6bed43b71fa62c5eed070593957ca
Lior Bruder <lior@11sheep.com>
2016-08-17 13:24:25 +0300

    Demo is ready

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,74 +1,72 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-// Wallpapers from: http://wallpapercave.com/cartoon-background
-
-// Landscape
-//
-// 1024x768 - 4:3
-// 1920x1080 - 16:9
-// 960x640 - 3:2
-// 1024x600 - 17:10
-// 1280x800 - 16:10
-//
-// Portrait
-// 
-// 800x1280 - 10:16
-
 public class MainScript : MonoBehaviour
 {
-
+    // UI
     private Dropdown mGameBKDropdown;
     private Dropdown mDeviceDropdown;
     private Dropdown mOrientationDropdown;
+    private Toggle mFitToFrame;
+
+    // Game objects
     private GameObject mDemoGame;
     private SpriteRenderer mDemoGameSpriteRenderer;
     private GameObject mDeviceFrame;
     private GameObject mJumpingBall;
+    private GameObject mButtomBar;
+
+    // Local variables 
     private PhoneDetails mCurrentDevice;
-    private Toggle mFitToFrame;
 
     private List<BackgroundPicture> mLandscapeBackgroundsList = new List<BackgroundPicture>
     {
-        new BackgroundPicture() { isLandscape = true, dislayName = "960x640 (3:2)", resourceName = "bk/landsscape_960x640_bk", ratio = (3f/2f) },
+        new BackgroundPicture() { isLandscape = true, dislayName = "1920x1080 (16:9)", resourceName = "bk/landsscape_1920x1080_bk", ratio = (16f/9f) },
         new BackgroundPicture() { isLandscape = true, dislayName = "1024x600 (17:10)", resourceName = "bk/landsscape_1024x600_bk", ratio = (17f/10f) },
-        new BackgroundPicture() { isLandscape = true, dislayName = "1024x768 (4:3)", resourceName = "bk/landsscape_1024x768_bk" , ratio = (4f/3f) },
         new BackgroundPicture() { isLandscape = true, dislayName = "1280x800 (16:10)", resourceName = "bk/landsscape_1280x800_bk", ratio = (16f/10f) },
-        new BackgroundPicture() { isLandscape = true, dislayName = "1920x1080 (16:9)", resourceName = "bk/landsscape_1920x1080_bk", ratio = (16f/9f) }
+        new BackgroundPicture() { isLandscape = true, dislayName = "960x640 (3:2)", resourceName = "bk/landsscape_960x640_bk", ratio = (3f/2f) },
+        new BackgroundPicture() { isLandscape = true, dislayName = "1024x768 (4:3)", resourceName = "bk/landsscape_1024x768_bk" , ratio = (4f/3f) }
     };
 
     private List<BackgroundPicture> mPortraitBackgroundsList = new List<BackgroundPicture>
     {
-        new BackgroundPicture() { isLandscape = false, dislayName = "640x960 (2:3)", resourceName = "bk/portrait_640x960_bk", ratio = (2f/3f) },
+        new BackgroundPicture() { isLandscape = false, dislayName = "1080x1920 (9:16)", resourceName = "bk/portrait_1080x1920_bk", ratio = (9f/16f) },
         new BackgroundPicture() { isLandscape = false, dislayName = "600x1024 (10:17)", resourceName = "bk/portrait_600x1024_bk", ratio = (10f/17f) },
-        new BackgroundPicture() { isLandscape = false, dislayName = "768x1024 (3:4)", resourceName = "bk/portrait_768x1024_bk", ratio = (3f/4f) },
         new BackgroundPicture() { isLandscape = false, dislayName = "1800x1280 (10:16)", resourceName = "bk/portrait_800x1280_bk", ratio = (10f/16f) },
-        new BackgroundPicture() { isLandscape = false, dislayName = "1080x1920 (9:16)", resourceName = "bk/portrait_1080x1920_bk", ratio = (9f/16f) }
+        new BackgroundPicture() { isLandscape = false, dislayName = "640x960 (2:3)", resourceName = "bk/portrait_640x960_bk", ratio = (2f/3f) },
+        new BackgroundPicture() { isLandscape = false, dislayName = "768x1024 (3:4)", resourceName = "bk/portrait_768x1024_bk", ratio = (3f/4f) }
     };
 
     private List<PhoneDetails> mPhoneDetailsList = new List<PhoneDetails>
     {
-        new PhoneDetails() { phoneName = "iPhone 6S Plus (1920x1080, 19:6)", resourceName = "phone_frame_iphone_6s", xOffset = 59, yOffset = 15, width = 353, height = 200 },
+        new PhoneDetails() { phoneName = "Galaxy Note 5 (2560x1440, 16:9)", resourceName = "phone_frame_galaxy_note_5", xOffset = 39, yOffset = 8, width = 349, height = 197 },
+        new PhoneDetails() { phoneName = "Nexus 5X (1920x1080, 16:10)", resourceName = "phone_frame_nexus5x", xOffset = 46, yOffset = 12, width = 378, height = 213 },
+        new PhoneDetails() { phoneName = "Nexus 4 (1280x768, 16:10)", resourceName = "phone_frame_nexus4", xOffset = 72, yOffset = 17, width = 473, height = 284 },
+        new PhoneDetails() { phoneName = "iPad Mini 5 (1024x768, 4:3)", resourceName = "phone_frame_ipad_mini", xOffset = 49, yOffset = 17, width = 383, height = 288 },
+        new PhoneDetails() { phoneName = "iPhone 5S (1136x640, 16:9)", resourceName = "phone_frame_iphone_5s", xOffset = 82, yOffset = 22, width = 425, height = 242 },
+        new PhoneDetails() { phoneName = "iPhone 6S Plus (1920x1080, 16:9)", resourceName = "phone_frame_iphone_6s", xOffset = 59, yOffset = 15, width = 353, height = 200 },
         new PhoneDetails() { phoneName = "iPad AIR 2 (2048x1536, 4:3)", resourceName = "phone_frame_ipad_air2", xOffset = 50, yOffset = 25, width = 473, height = 355 }
     };
 
-    // x:113, y:35   w:762,h:584
-
     void Start()
     {
+        //
+        // Get references to all objects
+        //
         mDemoGame = GameObject.Find("DemoGame");
         mDemoGameSpriteRenderer = mDemoGame.GetComponent<SpriteRenderer>();
         mDeviceFrame = GameObject.Find("DeviceFrame");
         mJumpingBall = GameObject.Find("JumpingBall");
         mFitToFrame = GameObject.Find("FitToFrame").GetComponent<Toggle>();
-
+        mButtomBar = GameObject.Find("ButtomBar");
         mGameBKDropdown = GameObject.Find("GameBKDropdown").GetComponent<Dropdown>();
         mOrientationDropdown = GameObject.Find("OrientationDropdown").GetComponent<Dropdown>();
-
-        // Set the devices information
         mDeviceDropdown = GameObject.Find("DeviceDropdown").GetComponent<Dropdown>();
+
+        //
+        // Set the devices information
+        //
         mDeviceDropdown.ClearOptions();
 
         List<string> devicesList = new List<string>();
@@ -80,6 +78,10 @@ public class MainScript : MonoBehaviour
 
         mDeviceDropdown.AddOptions(devicesList);
 
+        //
+        // Initialize values
+        //
+
         // Fake set device
         OnDeviceValueChnaged(0);
 
@@ -88,13 +90,6 @@ public class MainScript : MonoBehaviour
 
         // Fake set game background
         OnBackgroundImageValueChange(0);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void OnFitToGameValueChange(bool isOn)
@@ -223,25 +218,27 @@ public class MainScript : MonoBehaviour
             }
         }
 
+        // Place the bottom bar in place
+        mButtomBar.transform.localPosition = new Vector2(0, -mDemoGameSpriteRenderer.bounds.size.y / 2);
+
         mDemoGame.transform.localScale = new Vector2(xScale, yScale);
+   
+        //
+        // Fit the ball to the screen size
+        //
 
-        float mJumpingBallWidth = mJumpingBall.GetComponent<SpriteRenderer>().bounds.size.x;
-        float mJumpingBallHeight = mJumpingBall.GetComponent<SpriteRenderer>().bounds.size.y;
+        // Stop the ball physics
+        mJumpingBall.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
-        Debug.Log("mJumpingBallWidth: " + mJumpingBallWidth + ", mJumpingBallHeight: " + mJumpingBallHeight);
+        // Put the ball in the center
+        mJumpingBall.transform.localPosition = Vector2.zero;
 
+        // Set the ball size relative to the screen width
+        mJumpingBall.transform.localScale = Vector2.one;
+        float jumpingBallWidth = mJumpingBall.GetComponent<SpriteRenderer>().bounds.size.x;
         float newDemoWidth = mDemoGameSpriteRenderer.bounds.size.x;
-
-        float mJumpingBallScaleX = (newDemoWidth / 10f) / mJumpingBallWidth;
-        //float mJumpingBallScaleY = (newDemoHeight / 10f) / mJumpingBallHeight;
-
+        float mJumpingBallScaleX = (newDemoWidth / 10f) / jumpingBallWidth;
         mJumpingBall.transform.localScale = new Vector2(mJumpingBallScaleX, mJumpingBallScaleX);
-
-        float mNewJumpingBallWidth = mJumpingBall.GetComponent<SpriteRenderer>().bounds.size.x;
-        float mNewJumpingBallHeight = mJumpingBall.GetComponent<SpriteRenderer>().bounds.size.y;
-
-        Debug.Log("mNewJumpingBallWidth: " + mNewJumpingBallWidth + ", mNewJumpingBallHeight: " + mNewJumpingBallHeight);
-
     }
 
     class BackgroundPicture
